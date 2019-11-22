e23b57fc5f68dc3144843a78db95cd19ffb01ae2
filipasenra <filipasenra4@gmail.com>
2019-11-22 22:57:16 +0000

    Organizing Code

--- AgendappDEMO/lib/Screens/Talk Screen/talkScreen.dart
+++ AgendappDEMO/lib/Screens/Talk Screen/talkScreen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_login_page/Components/displayAllThemes.dart';
 import 'package:flutter_login_page/Model/Talk.dart';
-import 'package:flutter_login_page/Model/ThemeTalk.dart';
 import 'package:intl/intl.dart';
 
 class TalkScreen extends StatefulWidget {
@@ -62,7 +62,7 @@ Widget informationTop(Talk talk) {
               talk.name,
               style: TextStyle(fontSize: 24.0, color: Colors.black),
             ),
-            showAllThemes(talk.themes),
+            displayAllThemes(talk.themes),
             Text(
               new DateFormat("EEE, MMM d 'at' HH:mm").format(talk.dateInitial),
               style: TextStyle(fontSize: 14.0, color: Colors.black),
@@ -72,47 +72,6 @@ Widget informationTop(Talk talk) {
               style: TextStyle(fontSize: 14.0, color: Colors.black),
             ),
           ]));
-}
-
-Widget showAllThemes(List<ThemeTalk> themes) {
-  return Container(
-      height: 76.0,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        physics: ClampingScrollPhysics(),
-        shrinkWrap: true,
-        itemCount: themes.length,
-        itemBuilder: (context, index) {
-          return showTheme(themes[index]);
-        },
-      ));
-}
-
-Widget showTheme(ThemeTalk theme) {
-  return Container(
-      padding: const EdgeInsets.only(right: 10, left: 0, top: 20, bottom: 20),
-      child: Container(
-        padding: const EdgeInsets.all(8.0),
-        decoration: BoxDecoration(
-            color: Colors.white,
-            border: Border.all(width: 2, color: theme.color),
-            borderRadius: BorderRadius.all(Radius.circular(5)),
-            boxShadow: [
-              BoxShadow(
-                  color: Color(0x20000000), blurRadius: 5, offset: Offset(0, 3))
-            ]),
-        child: Text(theme.name,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: theme.color,
-              shadows: [
-                Shadow(
-                  blurRadius: 5.0,
-                  color: Color(0xFFD8D8D8),
-                )
-              ],
-            )),
-      ));
 }
 
 Widget showDescription(Talk talk) {
