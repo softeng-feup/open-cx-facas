0310b8508dcc312405c197a8b9b896ecfb986c01
TigasMiller09 <tiagomiller09@gmail.com>
2019-11-23 01:20:42 +0000

    More practical schedule

--- AgendappDEMO/lib/Screens/Schedule_Screen/schedule_screen.dart
+++ AgendappDEMO/lib/Screens/Schedule_Screen/schedule_screen.dart
@@ -11,22 +11,52 @@ class MySchedulePage extends StatefulWidget {
   MySchedulePageState createState() => MySchedulePageState();
 }
 
-class MySchedulePageState extends State<MySchedulePage> {
+class MySchedulePageState extends State<MySchedulePage>   {
 
+  int firstBlock = 0;
+  int finalBlock = 0;
+  int numBlocks = 0;
+  double blockSize = 0.04;
+  ScrollController _controller1;
+  ScrollController _controller2;
 
   List<DateTime> daysList = [new DateTime(2019, 12, 8, 8, 0), new DateTime(2019, 12, 9, 8, 0),new DateTime(2019, 12, 10, 8, 0),new DateTime(2019, 12, 11, 8, 0),new DateTime(2019, 12, 12, 8, 0),new DateTime(2019, 12, 13, 8, 0)];
-
+  List<ScrollController> controllers = new List<ScrollController>();
 
   final List<String> timeInterval = [
     "08:00 AM", "08:30 AM", "09:00 AM", "09:30 AM", "10:00 AM", "10:30 AM", "11:00 AM", "11:30 AM", "12:00 AM", "12:30 AM",
     "01:00 PM", "01:30 PM", "02:00 PM", "02:30 PM", "03:00 PM", "03:30 PM", "04:00 PM", "04:30 PM", "05:00 PM", "05:30 PM", "06:00 PM"
   ];
 
-  int firstBlock = 0;
-  int finalBlock = 0;
-  int numBlocks = 0;
+  @override
+  void initState() {
 
+    super.initState();
+    createControllers();
+    _controller1 = ScrollController();
+    _controller1.addListener(listener1);
+    _controller2 = ScrollController();
+    _controller2.addListener(listener2);
+  }
 
+  void listener1() {
+    for(int i = 0; i < controllers.length; i++){
+      controllers[i].jumpTo(_controller1.offset);
+    }
+  }
+
+  void listener2() {
+    _controller1.jumpTo(_controller2.offset);
+  }
+
+  void createControllers() {
+    for(int i = 0; i < daysList.length; i++){
+      controllers.add(new ScrollController());
+      controllers[i].addListener(listener1);
+    }
+  }
+
+  
 
   String convertWeekDay(int day){
     switch (day) {
@@ -114,8 +144,8 @@ class MySchedulePageState extends State<MySchedulePage> {
         child: Column(
           children: <Widget>[
             Container(
-              color: backColor,
-              height: MediaQuery.of(context).size.height * 0.04 * (firstBlock-finalBlockBefore),
+              color: backColor.withOpacity(0),
+              height: MediaQuery.of(context).size.height * blockSize * (firstBlock-finalBlockBefore),                              
               width: MediaQuery.of(context).size.width,
             ),
             GestureDetector(
@@ -126,7 +156,7 @@ class MySchedulePageState extends State<MySchedulePage> {
               },
               child: Container(
                 color: widget.talkList[j].color.withOpacity(0.5),
-                height: MediaQuery.of(context).size.height * 0.04 * numBlocks,
+                height: MediaQuery.of(context).size.height * blockSize * numBlocks,
                 width: MediaQuery.of(context).size.width,
                 child: Center(
                   child: Padding(
@@ -165,12 +195,12 @@ class MySchedulePageState extends State<MySchedulePage> {
         ],
       ),
       color: Colors.white,
-      height: MediaQuery.of(context).size.height * 0.04,
+      height: MediaQuery.of(context).size.height * blockSize,
       width: MediaQuery.of(context).size.width,
     );
   }
 
-  Container displaySchedule(int i){
+  Widget displaySchedule(int i){
     Color color1;
     Color color2;
 
@@ -214,24 +244,38 @@ class MySchedulePageState extends State<MySchedulePage> {
               ],
             ),
             color: color1,
-            height: MediaQuery.of(context).size.height * 0.08,
+            height: MediaQuery.of(context).size.height * 2*blockSize,
           ),
-          for(int j = 0; j < widget.talkList.length; j++)
-            if(daysList[i].day == widget.talkList[j].dateInitial.day && widget.talkList[j].selected)
-              placeBlocks(i, j, color2, Color.fromARGB(255,247,220,222)),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * 0.7,
+            color: color2,
+            child: ListView(
+              scrollDirection: Axis.vertical,
+              controller: controllers[i],
+              children: <Widget>[
+                for(int j = 0; j < widget.talkList.length; j++)
+                  if(daysList[i].day == widget.talkList[j].dateInitial.day && widget.talkList[j].selected)
+                    placeBlocks(i, j, color2, Color.fromARGB(255,247,220,222)),
+                Container(
+                  color: Colors.black.withOpacity(0),
+                  height:MediaQuery.of(context).size.height * blockSize * (21-finalBlock),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
+      
       alignment: Alignment.topLeft,
       color: color2,
       width: (MediaQuery.of(context).size.width - (MediaQuery.of(context).size.width * 0.15)) * 0.3333,
     );
-
   }
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-        child: Container(
+    return Container(
             height: MediaQuery.of(context).size.height * 0.92,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -241,10 +285,21 @@ class MySchedulePageState extends State<MySchedulePage> {
                     children: <Widget> [
                       Container(
                         color: Colors.white,
-                        height: MediaQuery.of(context).size.height * 0.08,
+                        height: MediaQuery.of(context).size.height * 2*blockSize,
                       ),
-                      for(int i = 0; i < timeInterval.length; i++)
-                        displayTime(i),
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context).size.height * 0.7,
+                        color: Colors.white,
+                        child: ListView(
+                          controller: _controller1,
+                          scrollDirection: Axis.vertical,
+                          children: <Widget>[
+                            for(int i = 0; i < timeInterval.length; i++)
+                            displayTime(i),
+                          ],
+                        ),
+                      ),
                     ],
                   ),
                   alignment: Alignment.topLeft,
@@ -255,19 +310,42 @@ class MySchedulePageState extends State<MySchedulePage> {
                 Container(
                   width: MediaQuery.of(context).size.width * (1- 0.15),
                   height: MediaQuery.of(context).size.height,
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
+                  child: Stack(
                     children: <Widget>[
-                      for(int i = 0 ; i < daysList.length ; i ++)
-                        displaySchedule(i),
+                      ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: <Widget>[
+                          for(int i = 0 ; i < daysList.length ; i ++)
+                           displaySchedule(i),
+                        ],
+                      ),
+                     Container(
+                       child: ListView(
+                        controller: _controller2,
+                        scrollDirection: Axis.vertical,
+                        children: <Widget>[
+                           Container(
+                             color: Colors.blue.withOpacity(0),
+                             width: MediaQuery.of(context).size.width,
+                            height: MediaQuery.of(context).size.height * blockSize * 21,
+                           ),
+                        ],
+                      ),
+                        margin: EdgeInsets.fromLTRB(0, 55, 0, 0),
+                        color: Colors.black.withOpacity(0),
+                        width: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context).size.height,
+                      ),
                     ],
                   ),
+                  
                 ),
                 
               ],
+              
             )
-        )
-    );
+            
+        );
 
   }
 }
