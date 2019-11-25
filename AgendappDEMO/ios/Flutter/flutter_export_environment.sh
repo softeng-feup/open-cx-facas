42c606359d76f3468a1d4ae64f921650fb10c943
Gustavo nRm <gustavonrm@gmail.com>
2019-11-25 21:00:13 +0000

    equeci-me de uma linha sry

--- AgendappDEMO/ios/Flutter/flutter_export_environment.sh
+++ AgendappDEMO/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/gustavonrm/Developer/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/gustavonrm/Desktop/open-cx-facas-2776c32e1db37711366ab14fedce079bc91f7e81/AgendappDEMO"
+export "FLUTTER_APPLICATION_PATH=/Users/gustavonrm/Desktop/ESOF/open-cx-facas/AgendappDEMO"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
