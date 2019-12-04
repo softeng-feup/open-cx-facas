a5b8ca0229aca4182dfb5bc905e156640623c648
filipasenra <filipasenra4@gmail.com>
2019-12-04 09:42:56 +0000

    Add user

---
+++ AgendappDEMO/lib/Model/User.dart
@@ -0,0 +1,17 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+import 'ThemeTalk.dart';
+
+class User {
+
+  String name;
+  String email;
+  List<ThemeTalk> preferredThemes;
+  String photo;
+
+  User(this.name, this.email, this.preferredThemes, this.photo);
+
+
+}
